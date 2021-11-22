7451515ffc1a416af6ea3f1db0b7c0f1f819eb33
PMART272 <pmart272@ford.com>
2021-11-22 08:49:36 +0100

    Including "fuzzy" search

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,18 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable, Subject} from "rxjs";
+import {Hero} from "../hero";
+import {debounceTime, distinctUntilChanged, switchMap} from "rxjs/operators";
+import {HeroService} from "../hero.service";
 
 @Component({
-  selector: 'app-hero-search',
-  templateUrl: './hero-search.component.html',
-  styleUrls: ['./hero-search.component.scss']
+    selector: 'app-hero-search',
+    templateUrl: './hero-search.component.html',
+    styleUrls: ['./hero-search.component.scss']
 })
 export class HeroSearchComponent implements OnInit {
 
-  constructor() { }
+    private searchTerms = new Subject<string>();
+    heroes$!: Observable<Hero[]>;
 
-  ngOnInit(): void {
-  }
+    constructor(private heroService: HeroService) {
+    }
 
-  search(value: string) {
-    
-  }
+    ngOnInit(): void {
+        this.heroes$ = this.searchTerms.pipe(
+            debounceTime(300),
+            distinctUntilChanged(),
+            switchMap(newValue => this.heroService.searchHeroes(newValue))
+        );
+    }
+
+    search(value: string): void {
+        value != '' ?
+            this.searchTerms.next(value) :
+            this.searchTerms.next();
+    }
+
 }
